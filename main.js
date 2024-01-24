264158a90d337140da8c2d5e592828724890e214
othstar <nikaotiko@gmail.com>
2024-01-24 23:15:03 +0400

    added logic in main.js and changed styles

--- main.js
+++ main.js
@@ -1,15 +1,96 @@
-// Slider Logic
+//////////////////////////////////////////////// Slider Logic
+const leftArrow = document.querySelector(".left-arrow");
+const rigthArrow = document.querySelector(".right-arrow");
+const photos = document.querySelectorAll(".photos");
+const dots = document.querySelectorAll(".dot");
+let i = 0;
 
-// Dropdown logic
+// Right arrow functionality
+function rightArrowClick() {
+  if (i == 2) {
+    photos[i].classList.add("hidden");
+    i = 0;
+    photos[i].classList.remove("hidden");
+  } else {
+    photos[i].classList.add("hidden");
+    photos[i + 1].classList.remove("hidden");
+    i++;
+  }
+}
+
+// Slide auto-switcher (left to right)
+setInterval(() => {
+  rightArrowClick();
+}, 3000);
+
+// Left arrow functionality
+function leftArrowClick() {
+  if (i == 0) {
+    photos[i].classList.add("hidden");
+    i = 2;
+    photos[i].classList.remove("hidden");
+  } else {
+    photos[i].classList.add("hidden");
+    photos[i - 1].classList.remove("hidden");
+    i--;
+  }
+}
+
+rigthArrow.addEventListener("click", rightArrowClick);
+leftArrow.addEventListener("click", leftArrowClick);
+
+// Dots functionality
+function dotsSwitcher(i) {
+  switch (i) {
+    case 0:
+      photos[0].classList.remove("hidden");
+      photos[1].classList.add("hidden");
+      photos[2].classList.add("hidden");
+      break;
+    case 1:
+      photos[0].classList.add("hidden");
+      photos[1].classList.remove("hidden");
+      photos[2].classList.add("hidden");
+      break;
+
+    case 2:
+      photos[0].classList.add("hidden");
+      photos[1].classList.add("hidden");
+      photos[2].classList.remove("hidden");
+      break;
+    default:
+      break;
+  }
+}
+
+for (let j = 0; j < dots.length; j++) {
+  dots[j].addEventListener("click", function () {
+    dotsSwitcher(j);
+  });
+}
+
+////////////////////////////////////////////// Dropdown logic
 const dropDown = document.querySelectorAll(".question");
 const questionContent = document.querySelectorAll(".question-content");
-
+const imgElement = document.querySelectorAll(".question img");
 for (let i = 0; i < dropDown.length; i++) {
   dropDown[i].addEventListener("click", function () {
     questionContent[i].classList.toggle("hidden");
+    questionContent[i].classList.toggle("border-bottom");
+    dropDown[i].classList.toggle("border-bottom");
+    // Rotate the image when the <p> tag is clicked
+    imgElement[i].style.transform = questionContent[i].classList.contains(
+      "hidden"
+    )
+      ? "rotate(0deg)"
+      : "rotate(180deg)";
     for (let j = 0; j < dropDown.length; j++) {
+      // Opening another question causes closing first one
       if (questionContent[i] != questionContent[j]) {
         questionContent[j].classList.add("hidden");
+        questionContent[i].classList.add("border-bottom");
+        dropDown[j].classList.add("border-bottom");
+        imgElement[j].style.transform = "rotate(0deg)"; // Reset rotation for other images
       }
     }
   });
